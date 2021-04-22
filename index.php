9db39209684a45526e3191f70418aa7775e130c5
xFoReX <m.garbina@wp.pl>
2021-04-22 11:46:14 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+    <?php
+        require('./class/Village.class.php');
+        session_start();
+        if(!isset($_SESSION['v']))
+        {
+            echo "Tworzę nową wioskę...";
+            $v = new Village();
+            $_SESSION['v'] = $v;
+            $deltaTime = 0;
+        }
+       else
+        {
+           $v = $_SESSION['v'];
+           $deltaTime = time() - $_SESSION['time'];
+        }
+
+        $v->gain($deltaTime);
+        
+        if(isset($_REQUEST['action']))
+        {
+            switch($_REQUEST['action'])
+            {
+                case 'upgradeBuilding':
+                    if($v->upgradeBuilding('woodcutter'))
+                        {
+                            echo "Ulepszono budynek drwala";
+                        } 
+                        else
+                        {
+                            echo "Nie udało się ulepszyć budynku drwala";
+                        }
+                    break;
+                    default:
+                        echo ' Nieprawidłowa zmienna "action"';
+            }
+        }
+
+
+
+
+        $_SESSION['time'] = time();
+        var_dump($v);
+        var_dump($_REQUEST);
+
+    ?>
+    </pre>
+    <a href="index.php?action=upgradeBuilding&building=woodcutter">
+    <button>Rozbuduj drwala</button>
+    </a>
+    <button>Rozbuduj kopalnie żelaza</button>
+</body>
+</html>
+
