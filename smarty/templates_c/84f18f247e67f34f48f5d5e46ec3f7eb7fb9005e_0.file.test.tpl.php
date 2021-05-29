6b6c6684aedde44032a0f6eff2e1de7e6233895a
xFoReX <72551350+xFoReXx@users.noreply.github.com>
2021-05-29 23:06:17 +0200

    smarty odc1

---
+++ smarty/templates_c/84f18f247e67f34f48f5d5e46ec3f7eb7fb9005e_0.file.test.tpl.php
@@ -0,0 +1,37 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-05-29 20:51:55
+  from 'C:\xampp\htdocs\GRA GŁÓWNE\smarty\templates\test.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_60b28d4b709550_33784269',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '84f18f247e67f34f48f5d5e46ec3f7eb7fb9005e' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\GRA GŁÓWNE\\smarty\\templates\\test.tpl',
+      1 => 1622314314,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_60b28d4b709550_33784269 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Cześć <?php echo $_smarty_tpl->tpl_vars['imie']->value;?>
+!
+</body>
+</html><?php }
+}
